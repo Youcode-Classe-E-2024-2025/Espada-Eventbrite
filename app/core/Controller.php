d5104fcfef4578a3103851b66720372ef82aba51
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-02-10 16:36:39 +0100

    Add base controller and view

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,3 +1,32 @@
 <?php
 
-?>
+namespace App\Core;
+
+abstract class Controller
+{
+    protected $db;
+    protected $view;
+    protected $security;
+    protected $session;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->view = new View();
+        $this->security = new Security();
+        $this->session = new Session();
+    }
+
+    /**
+     * Renders a view template with provided data
+     * 
+     * @param string $template Path to the template file
+     * @param array $data Data to be passed to the view
+     * @return string Rendered view content
+     */
+    protected function render(string $template, array $data = [])
+    {
+        $data['session'] = new Session();
+        return $this->view->render($template, $data);
+    }
+}
