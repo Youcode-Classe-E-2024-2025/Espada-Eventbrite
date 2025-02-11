10786cda51a3c12915d86888b5b7f554ed914536
anouar el barry <elbarryanwar37@gmail.com>
2025-02-11 17:11:46 +0100

    create functions to render data for (users /events/comments)

---
+++ app/controllers/back/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers\Back;
+use App\Core\Controller;
+class DashboardController extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    
+    public function index(){
+        echo $this->render("/back/index.html.twig");
+    }
+    public function showEvents(){
+        echo $this->render("/back/events.html.twig");
+    }
+    public function showUsers(){
+        echo $this->render("/back/users.html.twig");
+    }
+    public function showComments(){
+        echo $this->render("/back/comments.html.twig");
+    }
+}
