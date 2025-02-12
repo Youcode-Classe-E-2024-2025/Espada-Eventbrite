02037e8ae54f3820176393363872c27a04192b4e
anouar el barry <elbarryanwar37@gmail.com>
2025-02-12 15:55:51 +0100

    feat: implement event listing page - Add event list route - Fix event template path - Update event list layout

--- app/config/routes.php
+++ app/config/routes.php
@@ -28,6 +28,7 @@ $router->addRoute('GET', '/Organiser/test', [App\controllers\front\OrganiserDash
 
 
 $router->addRoute('GET', '/events/search', [App\controllers\front\EventController::class, 'search']);
+$router->addRoute('GET', '/events/list', [App\controllers\front\EventController::class, 'index']);
 $router->addRoute('POST', '/organiser/dash', [App\controllers\front\OrganiserDashController::class, 'index']);
 $router->addRoute('GET', '/reservation', [App\controllers\front\ReservationController::class, 'getBooking']);
 
