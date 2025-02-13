247e7c5408e8ed8522243d286bba98c337b5e95f
anouar el barry <elbarryanwar37@gmail.com>
2025-02-13 10:20:40 +0100

    render events data to events list

--- app/controllers/front/EventController.php
+++ app/controllers/front/EventController.php
@@ -4,22 +4,28 @@ namespace App\controllers\front;
 
 use App\core\Controller;
 use App\services\EventService;
-use App\repository\EventRepository;
+use App\repository\EvenmentRepository;
 use App\models\Event;
 
 class EventController extends Controller
 {
     private EventService $eventService;
+    private EvenmentRepository $evenmentRepository; 
 
     public function __construct()
     {
         parent::__construct();
         $this->eventService = new EventService();
+        $this->evenmentRepository = new EvenmentRepository();
     }
 
     public function index()
     {
-      echo $this->render('front/event/event-list.html.twig');
+      $events =  $this->evenmentRepository->getAll();
+      $data = [
+        'events' => $events
+      ];
+      echo $this->render('front/event/event-list.html.twig',$data);
     }
 
     public function search()
