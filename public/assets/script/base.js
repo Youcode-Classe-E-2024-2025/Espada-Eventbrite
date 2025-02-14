0dabe543928733c18df00e5e4977159663740d17
anouar el barry <elbarryanwar37@gmail.com>
2025-02-14 15:50:26 +0100

    create update event form

--- public/assets/script/base.js
+++ public/assets/script/base.js
@@ -1,24 +1,5 @@
 const notifBtn = document.getElementById('notif_btn');
 const notifPopup = document.getElementById('notifications-popup');
-const locationBtn = document.getElementById('location-btn');
-const locationModal = document.getElementById('location-modal');
-const closeLocationModal = document.getElementById('close-location-modal');
-const cancelLocation = document.getElementById('cancel-location');
-const confirmLocation = document.getElementById('confirm-location');
-const locationSearch = document.getElementById('location-search');
-const locationList = document.getElementById('location-list');
-
-  // Event Listeners
-  locationBtn.addEventListener('click', () => {
-    locationModal.classList.remove('hidden');
-    populateLocations();
-  });
-
-  [closeLocationModal, cancelLocation].forEach(el => {
-    el.addEventListener('click', () => {
-      locationModal.classList.add('hidden');
-    });
-  });
 
 if (notifBtn && notifPopup) {
     // Toggle notification popup
